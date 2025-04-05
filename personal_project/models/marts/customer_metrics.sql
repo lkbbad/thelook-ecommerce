07d2cd1530d103f7c3150d1f9f6538cca3e51c00
lkbbad <lkbustabad@gmail.com>
2025-04-05 14:44:24 -0400

    initial mart code

---
+++ personal_project/models/marts/customer_metrics.sql
@@ -0,0 +1,41 @@
+set @@dataset_project_id = 'round-music-451401-a5';
+set @@dataset_id = 'thelook_analytics';
+
+CREATE OR REPLACE TABLE 
+  `customer_metrics` AS
+WITH user_orders_agg AS (
+  SELECT
+    o.user_id
+    , SUM(i.product_retail_price) AS total_order_revenue
+    , SUM(i.product_cost) AS total_order_cost
+    , AVG(i.product_retail_price) AS avg_order_revenue
+    , AVG(i.product_cost) AS avg_order_cost
+    , COUNT(DISTINCT i.product_id) AS total_products_purchased
+  FROM user_orders o
+  LEFT JOIN order_items_enriched i ON o.order_id = i.order_id 
+  GROUP BY 
+    o.user_id
+  )
+SELECT
+  u.user_id
+  , COALESCE(COUNT(DISTINCT e.event_id), 0) AS total_events
+  , COALESCE(COUNT(DISTINCT e.session_id), 0) AS total_sessions
+  , MAX(e.event_created_at) AS last_event_dttm
+  , COALESCE(COUNT(DISTINCT v.product_id), 0) AS total_products_viewed
+  , MAX(o.order_created_at) AS last_order_dttm
+  , COALESCE(COUNT(DISTINCT o.order_id), 0) AS total_orders
+  , COALESCE(MAX(r.total_order_revenue), 0) AS total_order_revenue
+  , COALESCE(MAX(r.total_order_cost), 0) AS total_order_cost
+  , COALESCE(MAX(r.total_order_revenue - r.total_order_cost), 0) AS total_order_profit
+  , COALESCE(MAX(r.avg_order_revenue), 0) AS avg_order_revenue
+  , COALESCE(MAX(r.avg_order_cost), 0) AS avg_order_cost
+  , COALESCE(MAX(r.avg_order_revenue), 0) - MAX(r.avg_order_cost) AS avg_order_profit
+  , COALESCE(MAX(r.total_products_purchased), 0) AS total_products_purchased
+FROM 
+  stg_users u
+LEFT JOIN user_events e ON u.user_id = e.user_id
+LEFT JOIN events_product_views v ON u.user_id = v.user_id
+LEFT JOIN user_orders o ON u.user_id = o.user_id
+LEFT JOIN user_orders_agg r ON u.user_id = r.user_id
+GROUP BY 
+  u.user_id;
