0ca86ddab4dcc0a6cca8743a0f32f6f207c66a75
Brett Vogelsang <brett.vogelsang@sas.com>
2025-04-07 22:21:30 +0000

    Initial push for dot-formatted mart code

--- models/thelook_ecommerce/marts/product_performance.sql
+++ models/thelook_ecommerce/marts/product_performance.sql
@@ -3,18 +3,13 @@
 -- Used for: Analyzing product-level performance, identifying top sellers, low converters, and high return items.
 -- Source tables: stg_products, event_product_views, order_items_enriched
 
-set @@dataset_project_id = 'round-music-451401-a5';
-set @@dataset_id = 'thelook_analytics';
-
-CREATE OR REPLACE TABLE 
-  product_performance AS
 WITH product_views AS (
   SELECT 
     product_id
     , COALESCE(COUNT(DISTINCT event_id), 0) AS total_product_views
     , COALESCE(COUNT(DISTINCT session_id), 0) AS unique_product_views
   FROM
-    events_product_views
+    {{ ref('events_product_views') }}
   GROUP BY 
     product_id
 ),
@@ -29,7 +24,7 @@ product_sales_metrics AS (
   , COALESCE(COUNTIF(item_status = 'Returned'), 0) AS total_product_returns
   , SAFE_DIVIDE(SUM(product_sale_price), COUNT(DISTINCT item_id)) AS avg_sale_price
   FROM
-    order_items_enriched
+    {{ ref('order_items_enriched') }}
   GROUP BY 
     product_id
 )
@@ -50,8 +45,8 @@ SELECT
   , COALESCE(MAX(s.total_product_returns), 0) AS total_product_returns
   , COALESCE(SAFE_DIVIDE(MAX(s.total_product_returns), MAX(s.total_product_purchases)), 0) AS product_return_rate
 FROM
-  stg_products p
+  {{ ref('stg_products') }} p
 LEFT JOIN product_views v ON p.product_id = v.product_id
 LEFT JOIN product_sales_metrics s ON p.product_id = s.product_id
 GROUP BY 
-  p.product_id;
+  p.product_id
